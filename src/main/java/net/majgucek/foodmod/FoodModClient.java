56f14def4e5b0ee5c8f493977b8f841fada20705
MajGucek <maj.gucek@gmail.com>
2022-11-12 20:56:22 +0100

    Initial commit :skull:

---
+++ src/main/java/net/majgucek/foodmod/FoodModClient.java
@@ -0,0 +1,10 @@
+package net.majgucek.foodmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
